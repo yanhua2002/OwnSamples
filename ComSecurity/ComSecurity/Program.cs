9a1950fccfeee71768317a27ac519486ac99a34a
yanhua2002 <yanhua2002@msn.cn>
2016-06-26 01:23:48 +0800

    Added UI

---
+++ ComSecurity/ComSecurity/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System.IO;
+
+namespace ComSecurity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp = null, splitter = " , ", tempUninstall = null;
+            object displayName = null, uninstallString = null;
+            RegistryKey currentKey = null;
+            RegistryKey pregKey = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall");
+
+            try
+            {
+                foreach (string item in pregKey.GetSubKeyNames())
+                {
+                    currentKey = pregKey.OpenSubKey(item);
+                    displayName = currentKey.GetValue("DisplayName");
+                    uninstallString = currentKey.GetValue("UninstallString");
+                    if (displayName!=null)
+                    {
+                        tempUninstall = (uninstallString == null) ? "Null" : uninstallString.ToString();
+                        temp += System.Environment.MachineName + splitter + displayName.ToString() + splitter + tempUninstall + System.Environment.NewLine;
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            // string currentDir = System.Environment.CurrentDirectory + @"\aaa.txt";
+            string currentDir = @"\\localhost\Downloads\" + System.Environment.MachineName + @".txt";
+            FileStream fs = new FileStream(currentDir, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(temp);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
